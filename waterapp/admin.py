685432ce151b7b72f4b368da8e6cb0a4d932cb67
MorganWambulwa <morganwambulwa019@gmail.com>
2025-12-05 01:01:50 +0300

    Added user profile, legal pages and updated footer content

--- waterapp/admin.py
+++ waterapp/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import WaterSource, IssueReport, RepairLog
 from .forms import RepairLogForm, WaterSourceForm, IssueReportForm, AdminRepairLogForm
 
+
 @admin.register(WaterSource)
 class WaterSourceAdmin(admin.ModelAdmin):
+    form = WaterSourceForm
     list_display = ('name', 'source_type', 'status', 'is_verified', 'last_updated')
     list_filter = ('status', 'source_type', 'is_verified')
     search_fields = ('name', 'description')
 
 @admin.register(IssueReport)
 class IssueReportAdmin(admin.ModelAdmin):
+    form = IssueReportForm
     list_display = ('water_source', 'priority_level', 'is_resolved', 'reported_at')
     list_filter = ('is_resolved', 'priority_level')
 
@@ -18,5 +23,25 @@ class RepairLogAdmin(admin.ModelAdmin):
     form = AdminRepairLogForm
     list_display = ('water_source', 'technician', 'repair_date', 'cost')
     
+    class Media:
+        css = {
+            'all': ('waterapp/css/admin_custom.css',)
+        }
 
-    
+
+admin.site.unregister(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    class Media:
+        css = {
+            'all': ('waterapp/css/admin_custom.css',)
+        }
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        
+        if 'username' in form.base_fields:
+            form.base_fields['username'].help_text = ''
+            
+        return form
