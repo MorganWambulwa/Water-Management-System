a59893a8d4ff90470a72bbb08a3f7c809b3fa157
MorganWambulwa <morganwambulwa019@gmail.com>
2025-12-22 23:20:13 +0300

    Configure email settings for Render deployment

--- waterconnect/settings.py
+++ waterconnect/settings.py
@@ -138,10 +138,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'False') == 'True'
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
